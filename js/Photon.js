463d7409ade50088e41c75f80b81becc584086f4
Gabriel Moretti <flwbiel@gmail.com>
2017-12-04 16:37:45 -0200

    First commit

---
+++ js/Photon.js
@@ -0,0 +1,18 @@
+
+
+
+
+        //Carousel
+        $(document).ready(function () {
+            $('.carousel').carousel();
+        });
+        
+        
+        
+        
+        
+        
+        //ScrollSpy Barra Vertical
+        $(document).ready(function () {
+            $('.scrollspy').scrollSpy();
+        });
