6c3beff8da48debb832b21a097dcb5f6f710f2e4
Gabriel Moretti <flwbiel@gmail.com>
2017-12-06 01:38:00 -0200

    Finishing

--- js/Photon.js
+++ js/Photon.js
@@ -16,3 +16,20 @@
         $(document).ready(function () {
             $('.scrollspy').scrollSpy();
         });
+
+
+        //Google Maps
+        
+        function initMap() {
+          var uluru = {lat: -23.5366, lng: -46.6463};
+          var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 17,
+            center: uluru
+          });
+          var marker = new google.maps.Marker({
+            position: uluru,
+            map: map
+          });
+        }
+    
+  
